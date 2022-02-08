3e2ecbe7e282d99097f74a927ed344720c185469
neyluu <dwa2321@gmail.com>
2022-02-08 14:14:17 +0100

    Update index.js
    *non double click on fields
    *all winning combianations
    *arrays with circle and cross positions

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const fields = document.getElementsByTagName("td");
-console.log(fields);
+const winLayouts = {
+    0: [0, 1, 2],
+    1: [3, 4, 5],
+    2: [6, 7, 8],
+    3: [0, 3, 6],
+    4: [1, 4, 7],
+    5: [2, 5, 8],
+    6: [0, 4, 8],
+    7: [2, 4, 6]
+};
 
 let playerRound = 1;
+let circleFields = [];
+let crossFields = [];
 
 for(let i = 0; i < fields.length; i++)
 {
-    
-        fields[i].addEventListener("click", function write(){
-            // console.log(i + "działa");
-            if(playerRound%2 == 0)
-            {
-                fields[i].innerHTML = "x";
-                playerRound++;
-                // fields[i].removeEventListener("click", write());
-            }
-            else
-            {
-                fields[i].innerHTML = "o";
-                playerRound++;
-                // fields[i].removeEventListener("click", write());
-            }
+    fields[i].addEventListener("click", function write(){
+        // console.log(i + "działa");
+        if(playerRound%2 == 1)
+        {
+            fields[i].innerHTML = "x";
+            playerRound++;
             
-        }); 
+            crossFields.push(i);
+            console.log("xFields " + crossFields)
+            
+            fields[i].removeEventListener("click", write);
+        }
+        else
+        {
+            fields[i].innerHTML = "o";
+            playerRound++;
+           
+            circleFields.push(i);
+            console.log("cFields " + circleFields);
+           
+            fields[i].removeEventListener("click", write);
+        }
+    }); 
     
 }
