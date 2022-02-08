c576b26c743894c866250524f0958cb12bd7c71c
neyluu <dwa2321@gmail.com>
2022-02-08 15:57:45 +0100

    update2
    *function division
    *working win and draw checking

--- index.js
+++ index.js
@@ -1,43 +1,77 @@
 const fields = document.getElementsByTagName("td");
-const winLayouts = {
-    0: [0, 1, 2],
-    1: [3, 4, 5],
-    2: [6, 7, 8],
-    3: [0, 3, 6],
-    4: [1, 4, 7],
-    5: [2, 5, 8],
-    6: [0, 4, 8],
-    7: [2, 4, 6]
-};
+const winLayouts = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-let playerRound = 1;
-let circleFields = [];
-let crossFields = [];
+let playerRound = 0;
+let gameState = ["","","","","","","","",""];
 
-for(let i = 0; i < fields.length; i++)
+function winCheck(winLayouts, gameState, player) 
 {
-    fields[i].addEventListener("click", function write(){
-        // console.log(i + "działa");
-        if(playerRound%2 == 1)
-        {
-            fields[i].innerHTML = "x";
-            playerRound++;
-            
-            crossFields.push(i);
-            console.log("xFields " + crossFields)
-            
-            fields[i].removeEventListener("click", write);
+    for(let i = 0; i <=7; i++)
+    {
+        const winCondition = winLayouts[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
         }
-        else
-        {
-            fields[i].innerHTML = "o";
-            playerRound++;
-           
-            circleFields.push(i);
-            console.log("cFields " + circleFields);
-           
-            fields[i].removeEventListener("click", write);
+        if (a === b && b === c) {
+            document.write(player +" WIN")
+            break
         }
-    }); 
-    
+    }
 }
+
+function draw(x) 
+{
+    if(x == 9)
+    {
+        document.write("REMIS");
+    }
+}
+
+function playGame()
+{
+    for(let i = 0; i < fields.length; i++)
+    {
+        fields[i].addEventListener("click", function write(){
+            if(playerRound%2 == 0)
+            {
+                let player = "x";
+                fields[i].innerHTML = "x";
+                playerRound++;
+            
+                gameState[i] = player;
+                console.log(gameState);
+    
+                fields[i].removeEventListener("click", write);
+                winCheck(winLayouts, gameState, player);
+            }
+            else
+            {
+                let player="o"
+                fields[i].innerHTML = "o";
+                playerRound++;
+               
+                gameState[i] = player;
+                console.log(gameState);
+    
+                fields[i].removeEventListener("click", write);
+                winCheck(winLayouts, gameState, player);
+            }
+            
+            draw(playerRound);
+        });
+        
+    }
+}
+playGame();
