a9480d12b0692d661153e1f96b38428782ad4cd7
neyluu <dwa2321@gmail.com>
2022-02-15 13:48:22 +0100

    added reset button
    but not working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fields = document.getElementsByTagName("td");
 const result = document.getElementsByClassName("result");
+const reset = document.getElementsByClassName("reset");
 const winLayouts = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,8 +16,32 @@ let playerRound = 0;
 let gameState = ["","","","","","","","",""];
 let isWin = false;
 
-// nie działa
-function gameOver() 
+function gameReset()
+{
+    let resetButton = document.getElementsByClassName("reset-button")
+    resetButton[0].addEventListener("click", function(){
+        //nie działa
+        // for(let i = 0; i <= fields.length; i++)
+        // {
+        //     if(gameState != "")
+        //     {
+        //         fields[i].innerHTML = "";
+        //     }
+        // }
+        // gameState = ["","","","","","","","",""];
+        // playerRound = 0;
+        // isWin = false;
+        // console.log("resetPlaterRound " + playerRound);
+        // console.log("resetGameState " + gameState);
+        // console.log("resetIsWin " + isWin);
+        // playGame();
+        
+        //reset odświeża stronę
+        window.location.reload(true);
+    });
+}
+
+function gameOver(gameState) 
 {
     for(let i = 0; i < fields.length; i++)
     {   
@@ -25,9 +50,10 @@ function gameOver()
             fields[i].addEventListener("click", function endGame(){
                 fields[i].innerHTML = "";
             });
-        }
-        
+        }    
     }
+    reset[0].innerHTML = "<button class='reset-button'>RESET</button>"
+    gameReset();
 }
 
 function winCheck(winLayouts, gameState, player, trials) 
@@ -42,6 +68,7 @@ function winCheck(winLayouts, gameState, player, trials)
             continue;
         }
         if(a === b && b === c) {
+            //player sie zmienia po wygranej, do poprawy
             result[0].innerHTML = player + "WIN";
             isWin = true;
             gameOver(gameState);
