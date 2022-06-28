bb0c42a99e970a129d04a1237643ac177bc3f9b9
neyluu <dwa2321@gmail.com>
2022-06-28 19:34:48 +0200

    settings window wip

--- theme.js
+++ theme.js
@@ -1,13 +1,40 @@
-const body = document.getElementsByTagName("body");
+document.querySelector(".settings-svg").addEventListener("click", function(){
+    document.querySelector(".settings-window").classList.toggle("settings-wide");
+    
+    // setTimeout(function(){
+    //     document.querySelector(".settings-window").classList.toggle("settings-visible");
+    // }, 700);
+});
 
-let isDark = false;
-
-theme.addEventListener("click", function(){
-    body[0].classList.toggle("body-dt");
-    document.querySelectorAll('td').forEach(td => td.classList.toggle("cell-dt"));
+document.querySelector(".svg-settings").addEventListener("click", function(){
+    document.querySelector(".svg-settings").classList.toggle("svg-settings-rotate");
 });
 
 
-document.getElementsByClassName("settings")[0].addEventListener("mouseover", function(){
-    document.getElementsByClassName("aside-main")[0].classList.toggle("wide");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THEME
+// const body = document.getElementsByTagName("body");
+
+// let isDark = false;
+
+// theme.addEventListener("click", function(){
+//     body[0].classList.toggle("body-dt");
+//     document.querySelectorAll('td').forEach(td => td.classList.toggle("cell-dt"));
+// });
+
+
+// document.getElementsByClassName("settings")[0].addEventListener("mouseover", function(){
+//     document.getElementsByClassName("aside-main")[0].classList.toggle("wide");
+// });
