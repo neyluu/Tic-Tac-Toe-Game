aaa4d4cf1a41a5c95739b6efd06cecc236656683
neyluu <dwa2321@gmail.com>
2022-07-28 11:33:46 +0200

    winCombinations in other file

--- game.js
+++ game.js
@@ -1,58 +1,4 @@
-// import { winCombinations } from "./winCombinations.js";
-const winCombinations = {
-    c3: [
-         [0, 1, 2],
-         [3, 4, 5],
-         [6, 7, 8],
-         [0, 3, 6],
-         [1, 4, 7],
-         [2, 5, 8],
-         [0, 4, 8],
-         [2, 4, 6]
-    ],
-    c4: [
-         [0, 1, 2, 3],
-         [4, 5, 6, 7],
-         [8, 9, 10, 11],
-         [12, 13, 14, 15],
-         [0, 4, 8, 12],
-         [1, 5, 9, 13],
-         [2, 6, 10, 14],
-         [3, 7, 11, 15],
-         [0, 5, 10, 15],
-         [3, 6, 9, 12] 
-    ],
-    c5: [
-         [0, 1, 2, 3, 4],          
-         [5, 6, 7, 8, 9],         
-         [10, 11, 12, 13, 14],
-         [15, 16, 17, 18, 19],
-         [20, 21, 22, 23, 24],
-         [0, 5, 10, 15, 20],
-         [1, 6, 11, 16, 21],
-         [2, 7, 12, 17, 22],
-         [3, 8, 13, 18, 23],
-         [4, 9, 14, 19, 24],
-         [0, 6, 12, 18, 24],
-         [4, 8, 12, 16, 20]
-    ],
-    c6: [
-         [0, 1, 2, 3, 4, 5],          
-         [6, 7, 8, 9, 10, 11],
-         [12, 13, 14, 15, 16, 17],
-         [18, 19, 20, 21, 22, 23],
-         [24, 25, 26, 27, 28, 29],
-         [30, 31, 32, 33, 34, 35],
-         [0, 6, 12, 18, 24, 30],
-         [1, 7, 13, 19, 25, 31],
-         [2, 8, 14, 20, 26, 32],
-         [3, 9, 15, 21, 27, 33],
-         [4, 10, 16, 22, 28, 34],
-         [5, 11, 17, 23, 29, 35],
-         [0, 7, 14, 21, 28, 35],
-         [5, 10, 15, 20, 25, 30]
-    ]
-}
+import { winCombinations } from "./winCombinations.js";
 
 const result = document.getElementsByClassName("result");
 const reset = document.getElementsByClassName("reset");
@@ -69,8 +15,6 @@ let xPoints = 0, oPoints = 0;
 
 function gameReset()
 {
-    //reset odświeża stronę
-    // window.location.reload(true);
     reset[0].innerHTML = "";
 
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
@@ -94,7 +38,6 @@ function gameOver()
 
 function addPoints(player)
 {
-    // result[0].innerHTML = player + " WIN";
     result[0].innerHTML = "<p>Player " + player + " win!</p>";
     isWin = true;
     
@@ -122,10 +65,7 @@ function winCheck(gameState, player, trials)
             currentCombination.push(i);
         }
     }
-    // console.log(currentCombination);
     
-
-    //return zamiast isWin == false w if
     if(rows == 3)
     {
         for(let i = 0; i < winCombinations.c3.length; i++)
@@ -133,7 +73,6 @@ function winCheck(gameState, player, trials)
             if(currentCombination.includes(winCombinations.c3[i][0]) && currentCombination.includes(winCombinations.c3[i][1]) && currentCombination.includes(winCombinations.c3[i][2]))
             {
                 addPoints(player);
-                console.log("win");
                 return;
             }
         }
@@ -145,7 +84,6 @@ function winCheck(gameState, player, trials)
             if(currentCombination.includes(winCombinations.c4[i][0]) && currentCombination.includes(winCombinations.c4[i][1]) && currentCombination.includes(winCombinations.c4[i][2]) && currentCombination.includes(winCombinations.c4[i][3]))
             {
                 addPoints(player);
-                console.log("win");
                 return;
             }
         }   
@@ -157,7 +95,6 @@ function winCheck(gameState, player, trials)
             if(currentCombination.includes(winCombinations.c5[i][0]) && currentCombination.includes(winCombinations.c5[i][1]) && currentCombination.includes(winCombinations.c5[i][2]) && currentCombination.includes(winCombinations.c5[i][3]) && currentCombination.includes(winCombinations.c5[i][4]))
             {
                 addPoints(player);
-                console.log("win");
                 return;
             }
         }
@@ -169,7 +106,6 @@ function winCheck(gameState, player, trials)
             if(currentCombination.includes(winCombinations.c6[i][0]) && currentCombination.includes(winCombinations.c6[i][1]) && currentCombination.includes(winCombinations.c6[i][2]) && currentCombination.includes(winCombinations.c6[i][3]) && currentCombination.includes(winCombinations.c6[i][4]) && currentCombination.includes(winCombinations.c6[i][5]))
             {
                 addPoints(player);
-                console.log("win");
                 return;
             }
         }
@@ -190,21 +126,19 @@ function playGame()
     
     for(let i = 0; i < fields.length; i++)
     {
-        fields[i].classList.toggle("event"); //temp
-        
         fields[i].addEventListener("click", function write(){
             if(gameState[i] == "" && isWin == false)
             {
                 if(playerRound%2 == 0)
                 {
                     player = "x";
-                    fields[i].innerHTML = "<span class='x'>x</span>";
+                    fields[i].innerHTML = "<span class='x'>" + player + "</span>";
                     gameState[i] = player;
                 }
                 else
                 {
                     player="o"
-                    fields[i].innerHTML = "<span class='o'>o</span>";
+                    fields[i].innerHTML = "<span class='o'>" + player + "</span>";
                     gameState[i] = player;
                 }
                 playerRound++;
@@ -226,15 +160,10 @@ function generate()
         setTimeout(function(){
             document.getElementsByClassName("ttt-img")[0].style.display = "none"
         }, 500);
-        
-
-        //nie działa z nowym formem
-        // rows = parseInt(document.getElementById("rows").value);
 
         for(let i = 0; i < rows; i++)
         {
             document.getElementsByClassName("play-field")[0].innerHTML += "<tr class='play-tr'></tr>";
-            // let tr = document.getElementsByTagName("tr");
             let tr = document.getElementsByClassName("play-tr");
             
             for(let j = 0; j < rows; j++)
